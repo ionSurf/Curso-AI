529ed6bb0e5af15a1df9f2fa10839eca4c22b90f
Elihu Garrett <pmaekb@hotmail.com>
2016-06-12 18:54:15 -0500

    Update fugu_modal.lua

--- Clase-LM/fugu_modal.lua
+++ Clase-LM/fugu_modal.lua
@@ -1,38 +1,32 @@
 module(...,package.seeall)
 
 local m, vs, col
-local direc = "ruta de archivo"
+local direc = "ruta/de/archivo"
 local valores
 
 function setup()
-	m = dodecahedron()
+  -- primitivos que también pueden usarse: sphere(), cylinder(n), dodecahedron(), octahedron(), icosahedron(), cube(), cone(i,j)
+  m = dodecahedron()
   --m:subdivide(3)
-	fgu:add(meshnode(m))
-	vs = vertexlist(m)
+  fgu:add(meshnode(m))
+  vs = vertexlist(m)
 end
 
 function update(dt)
- 
   valores = leer(direc)
- 
-  if #valores > 0 then
-  
-    each(vs, function(v)
-        local pos = v:getPos()
-			  local  v.c = col(pos.y,pos.x,pos.z,fgu.t)
-    end)
-	
-    each(vs, function (v)
-        local pos = v.p
-        local p = perturb(tan(pos.x),pos.y,pos.z, fgu.t)		
-        local pv = random_vec3(sin((dt+p)*(valores)/100))
-        v.p = pos + pv
-        end)
-	
-  --print(value)
- 
+   if #valores > 0 then
+     each(vs, function(v)
+       pos = v:getPos()
+       v.c = col(pos.y,pos.x,pos.z,fgu.t)
+     end)
+     each(vs, function (v)
+       local pos = v.p
+       local p = perturb(tan(pos.x),pos.y,pos.z, fgu.t)		
+       local pv = random_vec3(sin((dt+p)*(valores)/100))
+       v.p = pos + pv
+     end)
+  --print(valores)
   end
-
 end
 
 leer = function (dir)
@@ -41,14 +35,14 @@ leer = function (dir)
  return lee
 end
 
-col = function(x,y,z,t) 
-	return vec3(
-		noise(x+cos(t),y/cos(t),z),
-		0,		
-		noise(x-cos(t),t/y,z+t)
-		) 
+col = function(x,y,z,t)
+  return vec3(
+    noise(x+cos(t),y/cos(t),z), -- rojo
+    0, -- verde		
+    noise(x-cos(t),t/y,z+t) -- azul
+    )
 end
 
 perturb = function(x,y,z,t) 
-	return noise(x*cos(t),y*tan(t),z+sin(t)) 
+  return noise(x*cos(t),y*tan(t),z+sin(t)) 
 end
